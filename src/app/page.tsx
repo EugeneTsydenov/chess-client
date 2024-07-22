fac1d091ac406a3820471362b79f2b4909823ecf
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-07-22 23:41:02 +0800

    _

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
-export default function Home() {
+import { http } from '@shared/lib';
+
+const test = async () => {
+  const res = await http.get<{ displayName: string }>({ url: '/users/4' });
+  console.log(res.data.displayName);
+};
+
+export default async function Home() {
+  await test();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       next
