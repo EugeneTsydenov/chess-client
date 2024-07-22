87631ddfb2a1f3d0c88532ff65558035e1468825
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-07-22 20:15:00 +0800

    configure: eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: ['next/core-web-vitals', 'eslint:recommended', 'prettier', '@feature-sliced', 'plugin:@conarti/feature-sliced/recommended'],
+  plugins: ['prettier', 'simple-import-sort'],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    "prettier/prettier": "error",
+    'max-len': 'off',
+    'consistent-return': 'off',
+    'no-shadow': 'off',
+    'no-param-reassign': 'warn',
+    'no-template-curly-in-string': 'off',
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // External packages:
+          ['^tsx', '^@?\\w'],
+          // Internal packages:
+          ['^@(siberiacancode-core/.*|$)'],
+          // Alias imports:
+          ['^@(([\\/.]?\\w)|assets|test-utils)'],
+          // Side effect imports:
+          ['^\\u0000'],
+          // Parent imports:
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports:
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports:
+          ['^.+\\.s?css$']
+        ]
+      }
+    ]
+  }
+}
