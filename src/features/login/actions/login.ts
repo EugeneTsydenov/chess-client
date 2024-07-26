919d05b68f268fc532c3863903b2adcb4ae872ce
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-07-26 20:43:47 +0800

    implement: login action

---
+++ src/features/login/actions/login.ts
@@ -0,0 +1,21 @@
+'use server';
+
+import { login, LoginFormSchemaType } from '@entities/auth';
+import { cookies } from 'next/headers';
+
+export const loginAction = async (credentials: LoginFormSchemaType) => {
+  const response = await login(credentials);
+  console.log(response.data);
+  const data = response.data;
+
+  if (response.ok) {
+    if (data.cookies) {
+      cookies().set(data.cookies.accessToken);
+      if (data.cookies.refreshToken) {
+        cookies().set(data.cookies.refreshToken);
+      }
+    }
+  }
+
+  return response;
+};
