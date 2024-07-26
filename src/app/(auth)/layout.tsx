e197990698f8eace2c4596dae58f1f713949418e
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-07-26 16:09:04 +0800

    layout: login page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,25 @@
 import { ComponentWithChildren } from '@shared/types';
+import { Card, Link, Logo } from '@shared/ui';
 import * as React from 'react';
 
 const Layout = ({ children }: ComponentWithChildren) => {
-  return <div>authenticated {children}</div>;
+  return (
+    <main className="bg-background h-screen">
+      <div className="container p-[0.7rem]">
+        <div className="flex items-center justify-center flex-col gap-10 pt-6">
+          <Link
+            href="/"
+            className="text-4xl font-bold tracking-tight hover:no-underline"
+          >
+            <Logo />
+          </Link>
+          <Card className="rounded-xl border bg-card text-card-foreground shadow p-6 w-full sm:w-2/3 lg:w-[42%]">
+            {children}
+          </Card>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Layout;
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,42 +1,8 @@
-import { AuthApi } from '@entities/auth';
-import { cookies } from 'next/headers';
+import { LoginForm } from '@features/login';
 import * as React from 'react';
 
-const login = async () => {
-  try {
-    const res = await AuthApi.login({
-      username: 'Messi',
-      password: 'Messi212_',
-      rememberMe: true,
-    });
-
-    if (res.ok) {
-      const data = res.data;
-      cookies().set(data.cookies.accessToken);
-      if (data.cookies.refreshToken) {
-        cookies().set(data.cookies.refreshToken);
-      }
-
-      return true;
-    }
-
-    return false;
-  } catch (e) {
-    return false;
-  }
-};
-
 const Page: React.FC = () => {
-  const action = async () => {
-    'use server';
-    await login();
-  };
-
-  return (
-    <form action={action}>
-      <button type="submit">login</button>
-    </form>
-  );
+  return <LoginForm />;
 };
 
 export default Page;
